c9b598e3e6f6ecd2a652b734dfae7e2500fb650d
DKFN <valere.tetelin@gmail.com>
2020-04-29 21:53:57 +0200

    Block on skip step

--- client/gui/src/Game/Etapes.tsx
+++ client/gui/src/Game/Etapes.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useMemo} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {IAppState, setStep} from "../redux/reducer";
 import _ from "lodash";
@@ -8,7 +8,8 @@ interface IEtape {
     id: number;
     name: string;
 }
-const etapes = [{
+
+export const etapes = [{
     id: 6,
     name: "Coque"
 }, {
@@ -42,23 +43,28 @@ export const Etapes = () => {
     console.log("ETAPES", currentStep);
 
     const maybeSelectedComponents = useSelector((appState: IAppState) => appState.game && appState.game.components);
+    const latestEtapes = useMemo( () => maybeSelectedComponents && Math.max(...maybeSelectedComponents.map(c => etapes.findIndex(e => e.id === c.idEtape))),
+        [maybeSelectedComponents]);
+    console.log("Latest etapes : ", latestEtapes);
 
     const onClick = (e: IEtape) => () => dispatch(setStep(e.id));
 
     return (
         <div className="tile is-parent is-3 is-vertical">
             {
-                etapes.map((e) => {
-                    const maybeSelectedComponent = maybeSelectedComponents && _.find(maybeSelectedComponents, (c) => c.idEtape === e.id)
+                etapes.map((e, k) => {
+                    const maybeSelectedComponent = maybeSelectedComponents && _.find(maybeSelectedComponents, (c) => c.idEtape === e.id);
                     const color = e.id === currentStep
                         ? 'is-success'
                         : maybeSelectedComponent ? 'is-dark' : 'is-warning'; // TODO: Check if component selected or if current step
                     const componentSelectedName = maybeSelectedComponent ? maybeSelectedComponent.name :''// TODO: Check if component selected
+                    const disabled = k - 1 > (latestEtapes || 0);
+
                     return (
                         <article
                             className={`tile is-child notification ${color}`}
-                            onClick={onClick(e)}
-                            style={{minHeight: 105, cursor: 'pointer'}}
+                            onClick={disabled ? () => {} : onClick(e)}
+                            style={{minHeight: 105, cursor: 'pointer', opacity: disabled ? 0.2 : 1}}
                         >
                             <p className="title">{e.name}</p>
                             <p className="subtitle">{componentSelectedName}</p>
