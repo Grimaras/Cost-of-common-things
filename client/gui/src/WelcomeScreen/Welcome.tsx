66ef2d23164bf42dc9af8b8a07178c001e291c12
Richard <Richard.la-roze@epita.fr>
2020-05-05 14:00:48 -0700

    add accent welcomescreen

--- client/gui/src/WelcomeScreen/Welcome.tsx
+++ client/gui/src/WelcomeScreen/Welcome.tsx
@@ -18,33 +18,33 @@ export const Welcome = () => {
             <div className="hero-body">
                 <div className="container">
                     <h1 className="title">
-                        Construire un Telephone
+                        Construire un Téléphone
                     </h1>
                     <h2 className="subtitle">
                         <button className="button is-success" onClick={goToObjectiveScreen}>
-                            Demarrer le jeu
+                            Démarrer le jeu
                         </button>
                     </h2>
                         <hr />
                         <div className="columns">
                             <div className="column">
-                                <h1 className="title"> Construisez un telephone</h1>
-                                Un télephone est composé de nombreux composants comme la batterie, l'écran et le processeur
-                                A chaque etape, choissez le composant qui correspond le mieux au télephone que vous devez construire !
+                                <h1 className="title"> Construisez un téléphone</h1>
+                                Un téléphone est composé de nombreux composants comme la batterie, l'écran et le processeur
+                                A chaque etape, choissez le composant qui correspond le mieux au téléphone que vous devez construire !
                             </div>
                             <div className="column">
                                 <h1 className="title"> Soyez Rapides</h1>
                                 Chaque seconde que vous passez à choisir vos composants augmente le budget de Recherche et Développement
                             </div>
                              <div className="column">
-                                <h1 className="title"> Entree ou haut de gamme</h1>
-                                 Dimensionner correctement son telephone est important. Construirez vous un télephone d'éntrée de gamme qui doit
-                                 se vendre beaucoup afin de rencontrer du succes ou alors un télephone haut de gamme qui compte sur un marche de niche?
+                                <h1 className="title"> Entrée ou haut de gamme</h1>
+                                 Dimensionner correctement son téléphone est important. Construirez vous un téléphone d'éntrée de gamme qui doit
+                                 se vendre beaucoup afin de rencontrer du succes ou alors un téléphone haut de gamme qui compte sur un marche de niche?
                             </div>
                             <div className="column">
                                 <h1 className="title"> Faites le bon choix</h1>
-                                Certains composants sont incompatibles avec les autres. Restez coherents, vous ne pourrez pas par exemple choisir
-                                un ecran xxl dans une toute petite coque.
+                                Certains composants sont incompatibles avec les autres. Restez cohérents, vous ne pourrez pas par exemple choisir
+                                un écran xxl dans une toute petite coque.
                             </div>
                         </div>
                     </div>
